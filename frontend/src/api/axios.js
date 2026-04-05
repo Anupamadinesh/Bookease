6ff192d8ef06084d1af8500a6d5707e8a391b2f0
Anupamadinesh <anupamadinesh31@gmail.com>
2026-04-05 23:54:33 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://bookease-1-jbfa.onrender.com",
 });
 
 export default instance;
